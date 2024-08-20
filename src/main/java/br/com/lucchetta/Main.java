dff9fd3d8b613c14bd0e9c4b971ea6cdf81cc486
Alexandre Luchetti <luchetti.92@gmail.com>
2024-08-19 21:44:21 -0300

    Inserindo o looping

--- src/main/java/br/com/lucchetta/Main.java
+++ src/main/java/br/com/lucchetta/Main.java
@@ -6,37 +6,50 @@ import java.util.Scanner;
 
 public class Main {
 
-    private Scanner scanner;
+    private final Scanner SCANNER;
     private int[][] matrix;
 
     public Main() {
-        this.scanner = new Scanner(System.in);
+        this.SCANNER = new Scanner(System.in);
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
-        main.start();
+        new Main().start();
     }
 
     private void start() {
-        this.matrix = generateMatrix();
-        List<Integer> result = executeSnail();
-        System.out.println("Resultado: " + result);
-        System.out.println("====================================");
+        boolean executing = true;
+
+        while (executing) {
+            System.out.println("========== SNAIL PROJECT ==========");
+            System.out.println("1 - Jogar");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opcao:");
+            int choice = this.SCANNER.nextInt();
+
+            if (choice == 1) {
+                System.out.print("\nDigite o tamanho N da matriz NxN: ");
+                int matrixLength = this.SCANNER.nextInt();
+
+                this.matrix = generateMatrix(matrixLength);
+                List<Integer> result = executeSnail();
+
+                System.out.println("\nResultado: " + result);
+                System.out.println("====================================");
+            } else {
+                executing = false;
+            }
+        }
     }
 
-    private int[][] generateMatrix() {
-        System.out.println("========== SNAIL PROJECT ==========");
-        System.out.print("Digite o tamanho N da matriz NxN: ");
-        int matrixLength = this.scanner.nextInt();
-
+    private int[][] generateMatrix(int matrixLength) {
         int[][] matrix = new int[matrixLength][matrixLength];
 
         System.out.println("Digite os elementos da matriz:");
         for (int i = 0; i < matrixLength; i++) {
             for (int j = 0; j < matrixLength; j++) {
                 System.out.print("Elemento [" + i + "][" + j + "]: ");
-                matrix[i][j] = scanner.nextInt();
+                matrix[i][j] = SCANNER.nextInt();
             }
         }
 
@@ -53,7 +66,6 @@ public class Main {
         int right = this.matrix[0].length - 1;
 
         while (top <= bottom && left <= right) {
-            // Percorre da esquerda para a direita na linha superior
             for (int i = left; i <= right; i++) {
                 result.add(this.matrix[top][i]);
             }
@@ -66,7 +78,6 @@ public class Main {
             right--;
 
             if (top <= bottom) {
-                // Percorre da direita para a esquerda na linha inferior
                 for (int i = right; i >= left; i--) {
                     result.add(this.matrix[bottom][i]);
                 }
@@ -74,7 +85,6 @@ public class Main {
             }
 
             if (left <= right) {
-                // Percorre de baixo para cima na coluna esquerda
                 for (int i = bottom; i >= top; i--) {
                     result.add(this.matrix[i][left]);
                 }
